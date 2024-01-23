04396c147f9c34512feb82746a7645544a1a2553
OmarAfet <OmarAlenezy2004@gmail.com>
2024-01-23 19:40:34 +0300

    Add Top Margin in the footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 	const year = new Date().getFullYear();
 
 	return (
-		<footer className="text-xs p-4 border-t flex text-muted-foreground whitespace-pre">
+		<footer className="text-xs p-4 border-t flex text-muted-foreground whitespace-pre mt-16">
 			&copy; {year}{" "}
 			<Link className="link" href="https://github.com/OmarAfet">
 				@OmarAfet
